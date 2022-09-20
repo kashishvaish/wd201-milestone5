07fff6d07a373442265e860712dd63363897286d
Kashish Vaish <kashishvaish.kv@gmail.com>
2022-09-21 00:08:27 +0530

    Fixed code

--- models/todo.js
+++ models/todo.js
@@ -67,7 +67,11 @@ module.exports = (sequelize, DataTypes) => {
 
     displayableString() {
       let checkbox = this.completed ? "[x]" : "[ ]";
-      return `${this.id}. ${checkbox} ${this.title} ${this.dueDate}`;
+      if (this.dueDate == new Date()) {
+        return `${this.id}. ${checkbox} ${this.title}`;
+      } else {
+        return `${this.id}. ${checkbox} ${this.title} ${this.dueDate}`;
+      }
     }
   }
   Todo.init(
